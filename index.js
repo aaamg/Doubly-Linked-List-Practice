ea9191cc0ecfe32613f27bd2dec29305d545752b
Andrew García <andrewmanuelgarcia@gmail.com>
2021-03-17 17:43:40 +0000

    Added methods

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ class LinkedList {
   add(value) {
     if(!this.head){
       this.head = this.tail = new Node(value)
-      
     } else {
       this.head.next = new Node(value)
       this.head.next.prev = this.head
@@ -39,5 +38,6 @@ let ll = new LinkedList()
 ll.add(10)
 ll.add(20)
 ll.add(30)
+ll.add(40)
 ll.getHead()
 ll.getTail()
